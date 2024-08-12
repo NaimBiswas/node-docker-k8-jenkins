ed5f451fe26ee463c771b8974ce6aa5907e155d3
NaimBiswas <nayeembiswas2@gmail.com>
2024-08-12 18:40:54 +0600

    Auto trigger test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 
 app.get("/", (req, res) => {
-    res.send("Server is up and running 1")
+    res.send("Server is up and running 3")
 })
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
