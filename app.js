2e07e150f2f6d4d072f885d90cf07d28e2be41ff
NaimBiswas <nayeembiswas2@gmail.com>
2024-08-13 12:23:37 +0600

    Retrieving system IP

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-
+app.set('trust proxy', true);
 
 
 app.get("/", (req, res) => {
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log('Client IP:', clientIp);
     res.status(200).json({
         message: "Hello Work from GitHub>Webhook>JenKins Pipeline>Docker>gCloud Artifact>GKE cluster",
+        ip:req?.ip,
+        xForwadedIp: clientIp
+
     })
 })
 app.listen(process.env.PORT, () => {
